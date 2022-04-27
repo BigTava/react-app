fbe0d2e025c0122687ac9864516278526051a1d8
Tiago Tavares <tiago.tavares@inesctec.pt>
2022-04-27 13:42:06 +0100

    tests hooks

--- src/App.js
+++ src/App.js
@@ -1,52 +1,37 @@
 import "./App.css";
-import React from "react";
-import image from "./image.png";
+import React, { useState, useEffect } from "react";
 
-function Header(props) {
-  console.log(props);
-  return (
-    <header>
-      <h1>{props.name}</h1>
-    </header>
-  );
-}
+function App({ login }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-function Main(props) {
-  return (
-    <section>
-      <p>Coding For the {props.adjective} World</p>
-      <img src={image} height={200} alt="random simpson" />
-      <ul style={{ textAlign: "left" }}>
-        {props.dishes.map((dish, i) => (
-          <li key={dish.id}>{dish.title}</li>
-        ))}
-      </ul>
-    </section>
-  );
-}
+  useEffect(() => {
+    fetch(`htps://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]);
 
-function Footer(props) {
-  return (
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  );
-}
+  if (loading) return <h1>Loading...</h1>;
+  if (error) {
+    return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  }
 
-const dishes = ["carbonara", "Minestrone", "salmon", "godfish"];
+  if (!data) {
+    return null;
+  }
 
-const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }));
-console.log(dishObjects);
-dishes.map((dish) => console.log(dish));
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="BigTava World" />
-      <Main adjective="amazing" dishes={dishObjects} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
-  );
+  if (data) {
+    return (
+      <div>
+        <h1>{data.name}</h1>
+        <p>{data.company}</p>
+        <img alt={data.login} src={data.avatar_url} />
+      </div>
+    );
+  }
 }
 
 export default App;
