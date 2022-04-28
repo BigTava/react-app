f6ed14d1ca79cdadfba59d6b4d569f440f3c452e
Tiago Tavares <tiago.tavares@inesctec.pt>
2022-04-28 16:43:18 +0100

    adds tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
+import { Checkbox } from "./Checkbox";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Selecting checkbox", () => {
+  const { getByLabelText } = render(<Checkbox />);
+  const checkbox = getByLabelText(/not checked/);
+  fireEvent.click(checkbox);
+  expect(checkbox.checked).toEqual(true);
 });
